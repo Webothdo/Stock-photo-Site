e12a54dbf2d8ea66bf8c689508e3cde2b58cf5d3
webothdo <joshuaogboloo@gmail.com>
2023-08-28 15:05:32 +0100

    First commit for photo stock site

---
+++ main.js
@@ -0,0 +1,37 @@
+let imgContainer = document.querySelector('.image-container')
+let loadMoreBtn = document.querySelector('.load-more')
+
+let page = 1
+
+loadMoreBtn.addEventListener('click', () => {
+    page += 1
+    showPhoto()
+})
+
+async function getPhoto(pagination) {
+    try {
+        let response = await fetch(`https:picsum.photos/v2/list?page=${pagination}`)
+        let data = await response.json()
+        console.log(data);
+        return data
+    } catch (error) {
+        alert(error.message)
+    }
+
+}
+
+async function showPhoto() {
+    let images = await getPhoto(page)
+
+    for (let i = 0; i < images.length; i++) {
+        const image = images[i];
+
+        let myImage = document.createElement('img')
+        myImage.setAttribute('src', image.download_url)
+
+        imgContainer.appendChild(myImage)
+
+    }
+}
+
+showPhoto()
